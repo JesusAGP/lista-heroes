ceb147c7673c219225348b32635817ecf2f6b183
JesusAGP <jesusagp2711@gmail.com>
2023-04-18 13:38:47 -0600

    first commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 12, name: 'Dr. Nice', points: 32},
+      { id: 13, name: 'Bombasto', points: 24 },
+      { id: 14, name: 'Celeritas', points: 62 },
+      { id: 15, name: 'Magneta', points: 56 },
+      { id: 16, name: 'RubberMan', points: 81 },
+      { id: 17, name: 'Dynama', points: 22 },
+      { id: 18, name: 'Dr. IQ', points: 43 },
+      { id: 19, name: 'Magma', points: 61 },
+      { id: 20, name: 'Tornado', points: 33 }
+];
